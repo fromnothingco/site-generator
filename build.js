cb8bb96b5a75e7e58f28ccb7593f63b8b2788120
Jonathan Hamilton <jonathan.hamilton@hedgehoglab.com>
2019-11-04 13:45:45 +0000

    rewrite based on config

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const ejs = require("ejs")
 const fs = require('fs');
 const util = require('util');
+const config = require('./routes.json')
 
 const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
@@ -9,45 +10,74 @@ const mkdir = util.promisify(fs.mkdir);
 const stat = util.promisify(fs.stat);
 const access = util.promisify(fs.access);
 
-const getDataFile = async (dir) => {
-  const file = `${__dirname}/src/pages/${dir}/data.json`
+const getContextFiles = async (ourdir) => {
   try {
-    await access(file)
+    return await readdir(ourdir)
+  } catch (e) {
+    return []
+  }
+}
+
+const createDir = async (ourdir) => {
+  try {
+    return await mkdir(ourdir, { recursive: true })
   } catch (e) {
     return false
   }
+}
 
-  return await readFile(file, 'utf8')
+const getLocalDataFile = async (ourdir) => {
+  try {
+    return await readdir(ourdir)
+  } catch (e) {
+    return false
+  }
+}
+
+const writeIndexPages = async (writepath, data) => {
+  try {
+    return await writeFile(writepath, data)
+  } catch (e) {
+    return false
+  }
 }
 
 const getFiles = async () => {
   try {
-    const files = await readdir(`${__dirname}/src/pages`)
-    files.forEach(async item => {
-      const outDir = `${__dirname}/dist/${item}`
-      try {
-        const file = await readFile(`${__dirname}/src/pages/${item}/index.html`, 'utf8')
-        const data = await getDataFile(item)
-        const output = ejs.render(file, Object.assign(JSON.parse(data), {
-          header: __dirname + '/src/partials/header.ejs',
-          footer: __dirname + '/src/partials/footer.ejs'
-        }))
-
-        if (item === "index") {
-          return await writeFile(`${__dirname}/dist/index.html`, output)
-        }
-
-        try {
-          await stat(outDir)
-          return await writeFile(`${outDir}/index.html`, output)
-        } catch (e) {
-          await mkdir(outDir, { recursive: true })
-          return await writeFile(`${outDir}/index.html`, output)
-        }
-      } catch (e) {
-        console.log("error", e)
-      }
-    })
+    const { contexts, globalData, partials } = config
+    const gdata = JSON.parse(await readFile(__dirname + globalData))
+    Object.keys(contexts)
+      .map(async dir => {
+        const outDir = `${__dirname}/dist${contexts[dir].baseRoute}`
+        //read directory of contexts (pages routes)
+        const files = await getContextFiles(`${__dirname}/src/contexts/${dir}`)
+        //basedir for the contexts
+        await createDir(`${outDir}`)
+        //create dir if it doesnt exist
+        // map over files in context dir
+        files.map(async item => {
+          // get local data file
+          const localData = await getLocalDataFile(`${__dirname}/src/contexts/${dir}/${item}/data.json`)
+          // get html files and render with ejs
+          const html = await ejs.renderFile(`${__dirname}/src/contexts/${dir}/${item}/index.html`, Object.assign(
+            {},
+            localData,
+            gdata,
+            // shold change this to an reduce to keep more modular 
+            {
+              header: __dirname + partials.header,
+              footer: __dirname + partials.footer
+            }
+          ))
+          // if its an index file create file at the base of the dir
+          if (item === "index") {
+            return await writeIndexPages(`${outDir}/index.html`, html)
+          }
+          // other wise create index file inside of directory
+          await createDir(`${outDir + '/' + item}`)
+          await writeIndexPages(`${outDir + '/' + item}/index.html`, html)
+        })
+      })
   } catch (e) {
     throw new Error(e)
   }
